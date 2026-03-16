514a0ac34e32ea391691817970ce37c444a96e8d
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-16 20:28:56 +0500

    Add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['typeorm', 'better-sqlite3'],
+  },
+};
+
+export default nextConfig;
