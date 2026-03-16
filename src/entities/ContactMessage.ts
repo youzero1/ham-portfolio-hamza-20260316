2d20831706bc93f357a46f2b552b431d5d7d98dd
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-16 20:29:07 +0500

    Add src/entities/ContactMessage.ts

---
+++ src/entities/ContactMessage.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('contact_messages')
+export class ContactMessage {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name!: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  email!: string;
+
+  @Column({ type: 'varchar', length: 500 })
+  subject!: string;
+
+  @Column({ type: 'text' })
+  message!: string;
+
+  @Column({ type: 'boolean', default: false })
+  read!: boolean;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
